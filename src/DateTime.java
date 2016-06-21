53f3d7403c953f3b6cc4f4559444615bed247b9e
voletidheeraj29 <voletidheeraj@gmail.com>
2016-06-21 16:21:27 +0530

    Update DateTime.java

--- src/DateTime.java
+++ src/DateTime.java
@@ -5,7 +5,7 @@ public class DateTime {
 	   public static void main(String args[]) {
 	      
 	       Date date = new Date();
-	        
+	        //printing date
 	       System.out.println(date.toString());
 	   }
 	}
